cd6b1538914f9eb6cfc32c78ed71a646f1f96577
Victor XH <victor.develop@outlook.com>
2022-06-12 16:25:13 +0000

    +point events

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+/*
+Graph.ts
+*/
 export type NodeID = string
 
 export type Conn = {
@@ -13,18 +16,59 @@ export type Node = {
 
 export type Graph = Node[]
 
-type NodeDrag = 'node.drag'
-type NodeClick = 'node.click'
-type NodeSelect = 'node.select'
-type NodesSelect = 'nodes.select'
+/*
+Events.ts
+*/
+type NodeDrag = 'node.dragged'
+type NodeClick = 'node.clicked'
+type NodeSelect = 'node.selected'
+type NodesSelect = 'nodes.selected'
+type BasicNodeEvents = NodeDrag | NodeClick | NodeSelect | NodesSelect
+type EnhancedNodeEvents = NodePointEvents
 
-export type GraphEditorEvent = NodeDrag | NodeClick | NodeSelect | NodesSelect
+type NodePointEventCallback = (args: {
+  from: Node,
+  to: Node
+}, err: Error) => void
 
+type NodeEventCallbackPayload = {
+  node: Node,
+  type: GraphEditorEvent
+}
+export type HandleNodeEvent = (e: GraphEditorEvent, callback: (p: NodeEventCallbackPayload, err: Error) => void) => void
+
+/*
+Events.NodePoint.ts
+*/
+type NodePointToEvent = 'node.pointed.to'
+type NodePointFromEvent = 'node.pointed.from'
+type NodePointEvents =  NodePointToEvent | NodePointFromEvent
+type NodePointEventPayload = NodeEventCallbackPayload & {
+  from: Node,
+  to: Node
+}
+
+type HandleNodePointEvent = (
+  e: NodePointEvents,
+  callback: (
+    p: NodePointEventPayload,
+    err: Error
+  ) => void
+) => void
+
+export type GraphEditorEvent = BasicNodeEvents | EnhancedNodeEvents
+
+/*
+Editor.ts
+*/
 export type EditorTrait = {
   render(g: Graph): void
-  on(e: GraphEditorEvent, callback: any): void
+  on: HandleNodeEvent | HandleNodePointEvent
 }
 
 export type EditorGraphLoader = {
   loadFromGithub(url: string): void
 }
+
+
+
